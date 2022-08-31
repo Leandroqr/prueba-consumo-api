0b28636fd28dbcee9c27bab3273a3a0428f1d5b3
Leandroqr <jleandroqr@gmail.com>
2022-08-30 21:33:48 -0500

    desarrollo filtro

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -1,23 +1,68 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getCountries } from "../services/api";
+import '../App.css'
 
 const Countries = () => {
 
     const [countries, setCountries] = useState([])
+    const [filteredCountries, setFilteredCountries] = useState([])
+    const inputRef = useRef()
 
-    useEffect( async () => {
+    useEffect( () => {
 
-        let data = await getCountries()
+        const getData = async () => {
+            let data = await getCountries()
+            setCountries(data)
+            setFilteredCountries(data)
+        }
 
-        console.log(data)
+        getData()
+    }, [countries])
 
-    }, [])
+    // useEffect(() => {
+        
+    // }, [countries])
     
 
+    function removeDiacritic(texto) {
+        return texto.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")
+    }
 
+    const filterCountries = (e) => {
+        let valueText = e.target.value.toUpperCase()
+
+        const newCountries = filteredCountries.filter( el => {
+            const searchParam = removeDiacritic(valueText)
+    
+            return (
+                removeDiacritic(el.Country).startsWith(searchParam) 
+                ||
+                removeDiacritic(el.Slug).startsWith(searchParam) 
+                );
+            }
+        )
+
+        setCountries(newCountries)
+
+        console.log(countries)
+        console.log(newCountries)
+    }
+    
     return (
         <>
-        
+            <div>
+                <h3>Filtrar:</h3>
+                <input  ref={inputRef} onKeyUp={filterCountries} />
+            </div>
+            <div className="cont__countries">
+                {
+                    countries?.map( (el,key) => (
+                        <div className="card__country" key={key}>
+                            {el.Country}
+                        </div>
+                    ))
+                }
+            </div>
         
         </>
     )
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,16 @@ const URL = 'https://api.covid19api.com/countries'
 export async function getCountries() {
     try {
         const { data } = await axios.get(URL);
-        console.log(data);
+        return data
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getDataCountry(country) {
+    try {
+        const { data } = await axios.get(`${URL}/${country}`);
         return data
     }
     catch (error) {
